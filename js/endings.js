900ac826c2842c4db743241fdcf49c8abf89b16a
01091113 <ezsh97@gmail.com>
2026-03-29 14:12:59 +0900

    Update endings.js

--- js/endings.js
+++ js/endings.js
@@ -1,7 +1,12 @@
-
 function checkEnding(){
- if(player.gradePoint < 10) return "제적";
- if(player.stress > 100) return "번아웃";
- if(player.turn > 56) return "졸업";
- return null;
+ if(player.gradePoint < 10) endGame("제적");
+ if(player.stress > 100) endGame("번아웃");
+ if(player.otaku > 95) endGame("오타쿠 엔딩");
+ if(player.love > 95) endGame("결혼 엔딩");
+ if(player.turn > 56) endGame("졸업 엔딩");
+}
+
+function endGame(text){
+ alert(text);
+ location.reload();
 }
