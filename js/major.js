e63da5753de5af7423633b2366d51f0bdea7bf4a
01091113 <ezsh97@gmail.com>
2026-03-29 14:11:29 +0900

    Create major.js

---
+++ js/major.js
@@ -0,0 +1,26 @@
+const colleges = {
+  "인문대": {
+    "국문과": {relationship:5, gradePoint:5},
+    "영문과": {career:5},
+    "사학과": {gradePoint:8},
+    "철학과": {stress:5}
+  },
+  "공대": {
+    "컴퓨터공학": {career:10, stress:5},
+    "전자공학": {gradePoint:5},
+    "기계공학": {stress:8},
+    "화학공학": {gradePoint:7}
+  },
+  "예술대": {
+    "영화과": {relationship:8},
+    "연극과": {love:5},
+    "디자인": {career:5},
+    "사진": {otaku:5}
+  },
+  "신학대": {
+    "신학": {stress:-5},
+    "선교": {relationship:5},
+    "종교학": {gradePoint:5},
+    "상담": {relationship:8}
+  }
+};
