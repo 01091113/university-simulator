ed380dc640380ddde08f6ed7dcb832d3d8e08c86
01091113 <ezsh97@gmail.com>
2026-03-29 14:12:08 +0900

    Refactor events array with updated structure and titles

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
-
-let events = [
- {title:"MT 공지", text:"과 MT 공지가 올라왔다.", effects:{relationship:5, stress:3}},
- {title:"시험기간", text:"시험기간이 시작됐다.", effects:{gradePoint:5, stress:5}},
- {title:"데이트", text:"데이트를 했다.", effects:{love:5, stress:-5}},
- {title:"밤샘 덕질", text:"밤새 덕질했다.", effects:{otaku:5, stress:-5}}
+const events = [
+ {title:"MT", text:"MT 공지가 올라왔다.", effect:{relationship:5, stress:5}},
+ {title:"시험기간", text:"시험기간이다.", effect:{gradePoint:5, stress:5}},
+ {title:"썸 시작", text:"썸을 타기 시작했다.", effect:{love:5}},
+ {title:"밤샘 덕질", text:"밤새 정주행했다.", effect:{otaku:5, stress:-5}},
+ {title:"입대 통지서", text:"입대 통지서가 왔다.", effect:{stress:10}}
 ];
